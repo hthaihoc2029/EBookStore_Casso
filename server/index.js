a57aff50f89dfa0151585ae40277af20c65264b7
hthaihoc2029 <98394189+hthaihoc2029@users.noreply.github.com>
2024-05-05 10:08:57 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const clientAPIRouter = require("./client-services/routes");
 const app = express();
 const port = process.env.PORT || 8080;
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://e-book-store-casso-client.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
