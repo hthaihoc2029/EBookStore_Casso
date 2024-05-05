b6746f03fbc921ca6d07f386ebca1e8e6b0537eb
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-05 09:22:17 +0700

    edit payment api

--- client/src/services/apiPayment.jsx
+++ client/src/services/apiPayment.jsx
@@ -4,7 +4,7 @@ const postLogin = (username, password) => {};
 const postRegister = (username, password, CCCD) => {};
 
 const postPayment = () => {
-  return instance.post("/api/client/payment");
+  return instance.post(`${process.env.BACKEND_URL}/api/client/payment`);
 };
 
 export { postPayment, postLogin, postRegister };
