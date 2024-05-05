24a20ee00f2c99f588078ef5a9534b57b5036a19
HOC HUYNH <hthaihoc2029@gmail.com>
2024-05-05 09:27:36 +0700

    log api call

--- client/src/services/apiPayment.jsx
+++ client/src/services/apiPayment.jsx
@@ -4,6 +4,9 @@ const postLogin = (username, password) => {};
 const postRegister = (username, password, CCCD) => {};
 
 const postPayment = () => {
+  console.log("====================================");
+  console.log(`${process.env.BACKEND_URL}/api/client/payment`);
+  console.log("====================================");
   return instance.post(`${process.env.BACKEND_URL}/api/client/payment`);
 };
 
